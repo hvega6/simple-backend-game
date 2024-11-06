0dc5ba76f6795ae8c1eb88bc56f8333c8bea04df
Hugo Vega <hugo17avid@gmail.com>
2024-11-06 12:36:31 -0800

    Update server.js

--- server.js
+++ server.js
@@ -1,17 +1,18 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const dotenv = require('dotenv');
-const { populateMockData } = require('./config'); // Import the function
-
-dotenv.config();
+import { testMflix } from './routes/test.js';
+import express from "express";
+import mongoose from 'mongoose';
+import 'dotenv/config';
+// import  populateMockData  from './config.js';
 
 const app = express();
 app.use(express.json());
 
-mongoose.connect(process.env.MONGODB_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
+// await testMflix();
+
+// mongoose.connect(process.env.MONGODB_URI, {
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true,
+// });
 
 mongoose.set('strictQuery', false);
 
@@ -19,18 +20,18 @@ const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 db.once('open', async () => {
   console.log('Connected to MongoDB');
-  await populateMockData(); // Call the function to populate mock data
+  // await populateMockData(); // Call the function to populate mock data
 });
 
 // Import routes
-const playerRoutes = require('./routes/playerRoutes');
-const gameSessionRoutes = require('./routes/gameSessionRoutes');
-const scoreRoutes = require('./routes/scoreRoutes');
+// const playerRoutes = require('./routes/playerRoutes');
+// const gameSessionRoutes = require('./routes/gameSessionRoutes');
+// const scoreRoutes = require('./routes/scoreRoutes');
 
 // Use routes
-app.use('/api/players', playerRoutes);
-app.use('/api/game-sessions', gameSessionRoutes);
-app.use('/api/scores', scoreRoutes);
+// app.use('/api/players', playerRoutes);
+// app.use('/api/game-sessions', gameSessionRoutes);
+// app.use('/api/scores', scoreRoutes);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
